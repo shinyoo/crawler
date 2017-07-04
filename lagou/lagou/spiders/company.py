a0e27d3131208faef2f4ef550451f107cdce649f
shinyoo <shinyoo.dzan@gmail.com>
2017-07-04 21:08:03 +0800

    some thing

--- lagou/lagou/spiders/company.py
+++ lagou/lagou/spiders/company.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy.loader import ItemLoader
+from lagou.items import CompanyItem
 
 class CompanySpider(scrapy.Spider):
     name = "company"
     allowed_domains = ["lagou.com"]
-    start_urls = ['http://lagou.com/']
+    start_urls = ['https://www.lagou.com/gongsi/86068.html']
 
     def parse(self, response):
-        pass
+		l = ItemLoader(item=CompnayItem(), response=response)
+		l.add_xpath('name', '//div[@class="product_name"]')
+		l.add_xpath('name', '//div[@class="product_title"]')
+		l.add_xpath('price', '//p[@id="price"]')
+		l.add_css('stock', 'p#stock]')
+		l.add_value('last_updated', 'today') # you can also use literal values
+		return l.load_item()    	
---
+++ lagou/lagou/ualist.py
@@ -0,0 +1,14 @@
+ualist = [
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A',
+	'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+	'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+	'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
+	'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1',
+	'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0',
+	'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.2 (KHTML, like Gecko) ChromePlus/4.0.222.3 Chrome/4.0.222.3 Safari/532.2',
+	'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36'
+
+
+]
